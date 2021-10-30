663db1d0f440b463af1fa9034008cc122d71f0d2
Zeaul Shuvo <Zeaul.Shuvo@yrbrands.com>
2021-10-30 14:30:26 +0600

    Adding a bash file change

--- install1.sh
+++ install1.sh
@@ -12,8 +12,8 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt-cache madison docker-ce
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
 sudo docker run hello-world -y
 sudo usermod -aG docker ubuntu 
