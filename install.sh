e762dd966b89bf94401adfc2e9a445f8a249424b
Zeaul Shuvo <Zeaul.Shuvo@yrbrands.com>
2021-10-30 16:11:15 +0600

    Adding a bash file change

--- install.sh
+++ install.sh
@@ -1,12 +1,37 @@
 #Setup laravel project with installing necessary components
 sudo apt-get update
+sudo apt install curl -y
 sudo apt-get install php -y
 sudo apt-get install php7.4
 sudo apt-get install php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
+cd /home/ubuntu
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
-cd app/
-sudo composer install
-cp .env.example .env
+cd /home/ubuntu/DevOps-Challenge/app/
+composer install
+composer update
 php artisan key:generate
+
+cd /home/ubuntu
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-cache madison docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+sudo docker run hello-world 
+sudo usermod -aG docker ubuntu 
+sudo service apache2 stop -y
+cd /home/ubuntu/DevOps-Challenge/
+sudo docker-compose up -d --build
--- install2.sh
+++
@@ -1,7 +0,0 @@
-#Stopping apache server so that we can run nginx server in 80 port
-sudo service apache2 stop -y
-sudo apt remove apache2 -y
-
-#dockerization of laravel project
-cd /home/ubuntu/DevOps-Challenge/app/
-sudo docker-compose up -d --build
