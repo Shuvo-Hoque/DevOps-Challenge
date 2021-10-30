264f449b2c0386f34869f764acae5b7513fe944f
Zeaul Shuvo <Zeaul.Shuvo@yrbrands.com>
2021-10-30 14:29:20 +0600

    Adding a bash file change

--- install.sh
+++ install.sh
@@ -3,13 +3,10 @@ sudo apt-get update
 sudo apt-get install php -y
 sudo apt-get install php7.4
 sudo apt-get install php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
-sudo curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
 cd app/
-sudo composer install -y
+sudo composer install
 cp .env.example .env
 php artisan key:generate
-cd ..
-cd ..
-cd ..
